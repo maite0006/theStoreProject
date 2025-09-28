938a511e8c03b7656801935c2f0e9e6b784c83c2
MT332575@fi365.ort.edu.uy <MT332575@fi365.ort.edu.uy>
2025-09-27 23:31:36 -0300

    prueba

--- store.LogicaNegocio/Entidades/Producto.cs
+++ store.LogicaNegocio/Entidades/Producto.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace store.LogicaNegocio.Entidades
 {
-    internal class Producto
+    public abstract class Producto
     {
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public string Descripcion { get; set; }
+        public int Stock { get; set; }
+        public bool Activo { get; set; }= true;
+        public DateTime FechaPublicacion { get; set; } = DateTime.Now;
+        public string Medidas { get; set; }
+       
+
     }
 }
